8abf9acfebadf6248139b3cebfb46e36fe80858c
Samuel Gabel <smlgbl@gmail.com>
2012-12-15 10:24:37 +0100

    Show another example job

---
+++ jobs/nexusAvailable.js
@@ -0,0 +1,35 @@
+var nodeio = require('node.io')
+var Job = {}
+
+Job.name = "Google Nexus 4 Availability"
+Job.msg = 'fetching from Play Store ...'
+Job.enabled = true
+Job.status = 'building'
+Job.interval = 1000*60*5
+Job.timestamp = ''
+Job.func = function( callback ) {
+	var job  = new nodeio.Job({
+		input:	false,
+		run:	function(){
+			this.getHtml( 
+				'https://play.google.com/store/devices/details?id=nexus_4_16gb',
+				function( err, $ ) {
+					if(err) {
+						callback(err, null)
+					} else {
+						var t = $('span.hardware-price-description').fulltext
+						callback( null, {
+							msg: t,
+							status: 'success',
+							timestamp: new Date()
+						})
+					}
+				}
+			)
+		}
+	})
+	
+	nodeio.start( job, { silent: true })
+}
+module.exports = Job
+
