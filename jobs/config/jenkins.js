77551f59532636b96a44b712d307eb4797bf6d61
Samuel Gabel <samuel.gabel@project-a.com>
2013-02-05 11:49:27 +0100

    Update jenkins config

--- jobs/config/jenkins.js
+++ jobs/config/jenkins.js
@@ -2,28 +2,24 @@ module.exports = [
 {
 	user: 'username',
 	pass: 'password',
-	url: 'http://jenkins.example.com/jenkins/job/DonutBranch',
+	url: 'http://jenkins.example.com/jenkins/',
+	jobs: [ 'Jobname_1', 'Jobname_2' ],
 	enabled: true,
 	interval: 30000
 },
 {
 	user: 'username',
 	pass: 'password',
-	url: 'http://jenkins.example.com/jenkins/job/DonutTrunk',
+	url: 'http://ci.example.com/',
+	jobs: 'all',
 	enabled: true,
 	interval: 30000
 },
 {
 	user: 'username',
 	pass: 'password',
-	url: 'http://ci.example.com/job/QA_BranchDeploy',
-	enabled: true,
-	interval: 30000
-},
-{
-	user: 'username',
-	pass: 'password',
-	url: 'http://ci.example.com/job/Unittests_Retail',
+	url: 'http://ci.example2.com/',
+	jobs: [ 'test', 'deploy' ],
 	enabled: true,
 	interval: 30000
 }
